7b852ba2f183bee591e37473df96f0ee9a3ee2f1
Luke <lrsorrenti@hotmail.com>
2024-05-07 07:42:40 -0500

    added more overview, and min.js version of jQuery

---
+++ 01_Intro/05_jQueryFilters.md
@@ -0,0 +1,4 @@
+# jQuery Filters
+
+jQuery filters are similar to css pseudo classes, we can select all the p tags that are the nth child of a parent element
+
--- 02_Using_jQuery/scripts/script.js
+++ 02_Using_jQuery/scripts/script.js
@@ -2,7 +2,14 @@
 //     alert('jquery loaded!')
 // })
 
-$('#lead-banner p').css({color: "red"})
-$('h3').css({border: "2px solid blue"})
-$('.wrapper').css({border: "1px solid red", paddingLeft: "20px"})
-$('#clients').css({border: "1px solid yellow"})
+// $('#lead-banner p').css({color: "red"})
+// $('h3').css({border: "2px solid blue"})
+// $('.wrapper').css({border: "1px solid red", paddingLeft: "20px"})
+// $('#clients').css({border: "1px solid yellow"})
+
+// $('header nav li:first').css({border: "2px solid blue"});
+// $('header nav li:last').css({border: "2px solid green"});
+// $('#contact li:first-child').css({border: "2px solid pink"})
+// $('#contact li:last-child').css({border: "2px solid gold"})
+
+$('h3').css({border: "2px solid pink"})
